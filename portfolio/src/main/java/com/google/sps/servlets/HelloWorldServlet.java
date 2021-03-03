981caa31749b5ad8df0689bf696beff2d7f5c1de
Jacinda <jacindasoto22@gmail.com>
2021-03-03 01:25:10 +0000

    Update button to fetch /hello

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -13,6 +13,6 @@ public class HelloWorldServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world! My name is Jacinda :)</h1>");
+    response.getWriter().println("Hello world! My name is Jacinda :)");
   }
 }
