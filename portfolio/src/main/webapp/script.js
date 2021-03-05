1ae348f2a9ba962224d419bb3cc45df538936b19
Jacinda <jacindasoto22@gmail.com>
2021-03-05 02:27:14 +0000

    Update: Read random thoughts from back-end. Web-console error fixed.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,3 +26,35 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+//function for servlets
+async function showSomething(){
+    const responseFromServer = await fetch('/hello');
+    const textFromResponse = await responseFromServer.text();
+
+    const servletContainer = document.getElementById('servlet1-container');
+    servletContainer.innerText = textFromResponse;
+}
+
+async function getRandomThoughts(){
+    //fetch function that will get info from /hello
+    const serverResponse = await fetch('/hello');
+
+    //parsing to json
+    const ranInfo = await serverResponse.json();
+    //gets a random element w/Math.random(): Error w/ranInfo.length in web console
+    const ranElement = ranInfo[Math.floor(Math.random()*ranInfo.length)];  
+
+    //connecting with index.html to print array of random info
+    const servletContainer = document.getElementById('servlet2-container');
+    servletContainer.innerText = ranElement;
+    //testing what will print to the web console w/ ranInfo.length: prints string instead of int
+    console.log(ranInfo[Math.floor(Math.random()*ranInfo.length)]);
+    
+    //printing random message in the website console
+    console.log([Math.floor(Math.random()*ranInfo.length())]);
+    //console.log("info.get(1)");
+    //console.log("info.get(2)");
+
+}
+
