9fbc4d71f17b4a837f02c97cc8fbf1e7f8cf7d93
Jacinda <jacindasoto22@gmail.com>
2021-03-11 17:39:39 +0000

    Update Contact Form: Contact form now asks the user to enter email and choose to request contact information.

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -12,14 +12,34 @@ public class FormHandlerServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-    // Get the value entered in the form.
+    //Get the value(s) entered in the form.
     String textValue = request.getParameter("text-input");
+    boolean contactEmail = Boolean.parseBoolean(getParameter(request, "contact-email", "false"));
+    boolean contactPhone = Boolean.parseBoolean(getParameter(request, "contact-phone", "false"));
+    boolean contactGit = Boolean.parseBoolean(getParameter(request, "contact-git", "false"));
 
-    // Print the value so you can see it in the server logs.
-    System.out.println("You submitted: " + textValue);
+    //Print what the user submitted/if they requested info in the server logs.
+    if(contactEmail || contactPhone || contactGit)
+        System.out.println("Submitted: " + textValue + "\nContact information was requested.");
+    else
+        System.out.println("Submitted: " + textValue);
 
-    // Write the value to the response so the user can see it.
-    response.getWriter().println("You submitted: " + textValue);
+    //Print what the user submitted/if they requested info
+    if(contactEmail || contactPhone || contactGit)
+        response.getWriter().println("You submitted: " + textValue + "\nYou requested to receive contact informtation.");
+    else
+        response.getWriter().println("You submitted: " + textValue);
 
+    //redirect user to the main page: figure out how user can see above info & then be redirected
+    //response.getWriter().println("You are now being redirected to the main page");
+    //response.sendRedirect("http://www.jsoto-sps-spring21.appspot.com/");
+  }
+
+  private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+    String value = request.getParameter(name);
+    if (value == null) {
+      return defaultValue;
+    }
+    return value;
   }
 }
